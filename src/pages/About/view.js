51926b3eb08c35df79287f078e5e7fd481702626
vishalgddam873 <vishal18@navgurukul.org>
2024-03-05 19:23:48 +0530

    Added About Section

--- src/pages/About/view.js
+++ src/pages/About/view.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faCode,
+	faServer,
+	faMobileAlt,
+} from '@fortawesome/free-solid-svg-icons';
 import './style.css';
 import AboutLogo from '../../assets/images/about.png';
 
@@ -17,7 +24,37 @@ export default (props) => (
 					/>
 				</div>
 				<div className='about-bottom-right-container'>
-					<h1>ABOUT</h1>
+					<div className='about-card-container'>
+						<FontAwesomeIcon icon={faCode} size='2x' />
+						<div className=''>
+							<h4>Frontend Developer</h4>
+							<p>
+								I'm frontend developer with experince in building responsive and
+								optimised site
+							</p>
+						</div>
+					</div>
+
+					<div className='about-card-container'>
+						<FontAwesomeIcon icon={faServer} size='2x' />
+						<div className=''>
+							<h4>Backend Developer</h4>
+							<p>
+								I have experience developing fast and optimised back-end system and APIs
+							</p>
+						</div>
+					</div>
+
+					<div className='about-card-container'>
+						<FontAwesomeIcon icon={faMobileAlt} size='2x' />
+						<div>
+							<h4>Mobile App Developer</h4>
+							<p>
+								I'm a mobile app developer with experience in building responsive and
+								optimized applications for small screens.
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
