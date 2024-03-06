91a30b24242282d726c982999780e6c4cebb0dc8
vishalgddam873 <vishal18@navgurukul.org>
2024-03-06 19:31:35 +0530

    Project Section added

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,7 +1,42 @@
 import ProjectsView from './view';
+import EmediskillLogo from '../../assets/images/emediskill.png';
+import CargoLogo from '../../assets/images/cargo411.jpeg';
+import ProfmedServicesLogo from '../../assets/images/profmedservices.png';
 
 const ProjectsPage = () => {
-	return <ProjectsView />;
+	return (
+		<ProjectsView
+			projects={[
+				{
+					logo: EmediskillLogo,
+					title: 'EmediSkill',
+					description:
+						'EmediSkill is a user-friendly web platform where doctors buy courses,watch videos, and earn certificates after exams—simplifying and enhancing their ongoing professional education.',
+					techList: ['React', 'Redux', 'Mongodb'],
+					demoLink: 'https://emediskill.com/',
+				},
+
+				{
+					logo: ProfmedServicesLogo,
+					title: 'Profmed Services',
+					description:
+						'Profmed Services Mobile App: Streamlining access to vital resources for healthcare professionals, providing a seamless and engaging experience.',
+					techList: ['React Native', 'Redux', 'NodeJs'],
+					demoLink:
+						'https://play.google.com/store/apps/details?id=com.profmed.services',
+				},
+				{
+					logo: CargoLogo,
+					title: 'Cargo411',
+					description:
+						'Cargo411 Mobile App: Facilitated shipment tracking for cargo companies, streamlining the process of sending shipments via trucks, ensuring efficient logistics and real-time tracking.',
+					techList: ['React Native', 'Redux', 'NodeJs'],
+					demoLink:
+						'https://play.google.com/store/apps/details?id=com.mycargo.cargo',
+				},
+			]}
+		/>
+	);
 };
 
 export default ProjectsPage;
--- src/pages/Projects/view.js
+++ src/pages/Projects/view.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './style.css';
-import EmediskillLogo from '../../assets/images/emediskill.png';
 
 export default (props) => (
 	<section className='project-main-container'>
@@ -8,33 +7,32 @@ export default (props) => (
 			<h1 className='project-title'>PROJECTS</h1>
 		</div>
 		<div className='project-bottom-container'>
-			<div className='project-card'>
-				<div className='card-top-container'>
-					<img
-						alt='project-img'
-						draggable='false'
-						src={EmediskillLogo}
-						className='project-pic'
-					/>
-				</div>
-				<div className='project-content'>
-					<h3>Emediskill</h3>
-					<p>
-						EmediSkill is a user-friendly web platform where doctors buy courses,
-						watch videos, and earn certificates after exams—simplifying and enhancing
-						their ongoing professional education.
-					</p>
-					<div className='tech-list'>
-						<h5 className='tech-title'>React</h5>
-						<h5 className='tech-title'>Redux</h5>
-						<h5 className='tech-title'>Mongodb</h5>
+			{props.projects.map((item) => (
+				<div className='project-card'>
+					<div className='card-top-container'>
+						<img
+							alt='project-img'
+							draggable='false'
+							src={item.logo}
+							className='project-pic'
+						/>
+					</div>
+					<div className='project-content'>
+						<h3>{item.title}</h3>
+						<p>{item.description}</p>
+						<div className='tech-list'>
+							{item.techList.map((tech) => (
+								<h5 className='tech-title'>{tech}</h5>
+							))}
+						</div>
+					</div>
+					<div className='button-container'>
+						<a href={item.demoLink} target='_blank'>
+							<button className='button'>Demo</button>
+						</a>
 					</div>
 				</div>
-				<div className='button-container'>
-					<button className='button'>Demo</button>
-					<button className='button'>Source Code</button>
-				</div>
-			</div>
+			))}
 		</div>
 	</section>
 );
